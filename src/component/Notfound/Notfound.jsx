0b46508ee09d2de150969af717cc607aebd46dc7
abdulrahman941 <abdulrahman.1999hemida@gmail.com>
2026-01-21 11:55:37 +0200

    first commit

--- src/component/Notfound/Notfound.jsx
+++ src/component/Notfound/Notfound.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { Button } from '@heroui/react';
-import {Error} from 'src/assets/21.webp'
+import Error from '../../../src/assets/21.webp';
 
 export default function Notfound() {
 
   function GoLogin(){
-    window.location.href="/"
+    window.location.href="http://localhost:5191/SocialApp/"
   }
   return <>
   <title>Error | Route Social</title>
   <div className="container mx-auto">
     <div className="mx-auto text-center h-screen py-3 flex-col gap-4">
     <div className="flex justify-center align-items-center">
-      <img src={Error} alt="Error" />
+      <img src={Error} alt={Error} />
     </div>
     <div className="">
      <h2>Page Not Found</h2>
